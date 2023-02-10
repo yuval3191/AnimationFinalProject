3509f8d210bddb328d9722051aae5b2b62ff5cf8
Omri Chen <53468085+omrichen70@users.noreply.github.com>
2023-02-10 15:04:54 +0200

    Create AnimationVisitor.h

---
+++ AnimationVisitor.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Visitor.h"
+#include "Camera.h"
+
+#include <utility>
+#include <memory>
+
+namespace cg3d
+{
+
+    class AnimationVisitor : public Visitor
+    {
+    public:
+        void Run(Scene* scene, Camera* camera) override;
+        void Visit(Model* model) override;
+       // void Init() override;
+        bool drawOutline = true;
+        float outlineLineWidth = 5;
+
+        Eigen::Vector4f outlineLineColor{ 1, 1, 1, 1 };
+
+    private:
+        void DrawOutline();
+
+        Scene* scene;
+    };
+
+} // namespace cg3d
+
