1b1438039b004badb5c0adf7a66a43690005604f
Omri Chen <53468085+omrichen70@users.noreply.github.com>
2023-02-10 15:04:09 +0200

    Create Renderer.h

---
+++ Renderer.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+#include "Viewport.h"
+#include "DrawVisitor.h"
+#include "GLFW/glfw3.h"
+#include "AnimationVisitor.h"
+
+
+namespace cg3d
+{
+
+class Renderer
+{
+public:
+    void AddViewport(const std::shared_ptr<Scene>& scene);
+    void AddViewport(const std::shared_ptr<Viewport>& viewport);
+    void RemoveViewport(const std::shared_ptr<Viewport>& remove);
+
+    void RenderViewport(Viewport* viewport);
+    void RenderViewport(Viewport* viewport, Visitor* visitor);
+    void RenderViewportAtPos(int x, int y, Visitor* visitor = nullptr);
+    void RenderAllViewports();
+    void Draw() { RenderAllViewports(); } // for clarity
+
+    void MouseCallback(int x, int y, int button, int action, int mods, int buttonState[]);
+    void ScrollCallback(int x, int y, int xoffset, int yoffset, int buttonState[]);
+    void CursorPosCallback(int x, int y, int buttonStates[]);
+    void KeyCallback(int x, int y, int key, int scancode, int action, int mods);
+    void CharCallback(int x, int y, unsigned int codepoint);
+    void WindowSizeCallback(int newWidth, int newHeight);
+
+    [[nodiscard]] inline int GetWindowWidth() const { return windowWidth; }
+    [[nodiscard]] inline int GetWindowHeight() const { return windowHeight; };
+    std::vector<std::shared_ptr<Viewport>> viewports;
+
+
+private:
+    Viewport* FindViewportAtPos(int x, int y);
+
+    DrawVisitor defaultVisitor;
+    AnimationVisitor animationVisitor;
+    Viewport* viewportAtMousePress = nullptr;
+    Viewport* viewportAtKeyPress = nullptr;
+    int windowWidth = 0, windowHeight = 0, xPress = 0, yPress = 0;
+    bool isMouseDragging = false;
+};
+
+} // namespace cg3d
