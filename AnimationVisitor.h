52dccff25f348f0124301275b15e5f2331783757
Omri Chen <53468085+omrichen70@users.noreply.github.com>
2023-02-16 10:23:17 +0200

    Update AnimationVisitor.h

--- AnimationVisitor.h
+++ AnimationVisitor.h
@@ -5,6 +5,8 @@
 
 #include <utility>
 #include <memory>
+#include "../../tutorial/YuvalNoamOmri/Movement.cpp"
+#include "../../tutorial/YuvalNoamOmri/CollisionDetection.cpp"
 
 namespace cg3d
 {
@@ -14,10 +16,13 @@ namespace cg3d
     public:
         void Run(Scene* scene, Camera* camera) override;
         void Visit(Model* model) override;
-       // void Init() override;
+        // void Init() override;
         bool drawOutline = true;
         float outlineLineWidth = 5;
+        Eigen::Vector3f oldTailPos;
+        Eigen::Vector3f basePos;
 
+        bool firstTime = true;
         Eigen::Vector4f outlineLineColor{ 1, 1, 1, 1 };
 
     private:
